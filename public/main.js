3b281ec86784402a695c2eb125736ee365ca9eb0
MaggieChafee <maggie.chafee@gmail.com>
2023-08-30 19:50:03 -0500

    basic structure and function completed

--- public/main.js
+++ public/main.js
@@ -1,8 +1,13 @@
 import 'bootstrap'; // import bootstrap elements and js
 import '../styles/main.scss';
+import htmlStructure from '../utils/htmlStructure';
+import { getJokeBtn } from '../components/buttons';
+import domEvents from '../components/events/domEvents';
 
 const init = () => {
-
+  htmlStructure();
+  getJokeBtn();
+  domEvents();
 };
 
 init();
